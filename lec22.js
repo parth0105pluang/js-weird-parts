1656cc566b069d262a62e23c76806f0b0fc6fb9e
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-02 16:47:46 +0530

    lec 22

---
+++ lec22.js
@@ -0,0 +1,19 @@
+/*
+premitive types
+undefined
+null
+boolean
+number(flt pnt.)
+string->' ' or " "
+symbol
+*/
+var a=2,b=3,c=4;
+a=b=c;
+console.log(a);
+console.log(b);
+console.log(c);
+/*
+4
+4
+4
+*/
