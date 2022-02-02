c5a7a2d9657ff3cd0025aec4e1e76994cf471620
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-02 18:15:28 +0530

    lec 31

---
+++ lec30.js
@@ -0,0 +1,8 @@
+var person = new Object();
+person["firstname"]="Parth";
+person["lastname"]="Sharma";
+console.log(person.firstname);
+console.log(person["firstname"]);
+person.address= new Object();
+person.address.street = "Random street adress";
+console.log(person.address);
