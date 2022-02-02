b3811f9ef84126df24a2ab337b04d57ed899b269
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-02 12:40:58 +0530

    Execution Context Inro

---
+++ lec9.js
@@ -0,0 +1,12 @@
+//Syntax Parser
+//Lexical Environment
+//Execution Context
+//Name/Value Pairs
+//Objects->Collection of name/value pairs.
+//Each execution context has a Global Object associated with it.
+//Execution Context->Glb Obj+Otr Env+Our Code
+//var and fun also get attached to global object.
+var  a = "Hello World";
+function b(){
+
+}
