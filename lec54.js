f767eb5494af1c1ca2847d95ca1dafd0a7e6ee6b
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-03 18:19:54 +0530

    lec 55

--- lec54.js
+++ lec54.js
@@ -11,9 +11,11 @@ var john ={
     lastname:"Doe"
 }
 john.__proto__=person;
-for(var prop in john){
-    if(john.hasOwnProperty(prop)){
-        console.log(prop+": "+john[prop]);
-    }
-    
+console.log(john.getFullName());
+console.log(john.firstname);
+var jane ={
+    firstname:"Jane"
 }
+jane.__proto__=person;
+console.log(jane.getFullName());
+console.log(john.firstname);
