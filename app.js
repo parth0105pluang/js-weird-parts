87e0b5375db036d6ef9950e7cab25a40caee9b63
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-02-04 16:51:23 +0530

    Lec 80

--- app.js
+++
@@ -1,7 +0,0 @@
-var g = G$("parth","sharma");
-g.greet().setLang('es').greet(true);
-$('#login').click(function(){
-    var loginGrtr = G$("John",'Doe');
-    $("#logindiv").hide();
-    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting',true).log();
-});
---
+++ lec80.js
@@ -0,0 +1,7 @@
+var g = G$("parth","sharma");
+g.greet().setLang('es').greet(true);
+$('#login').click(function(){
+    var loginGrtr = G$("John",'Doe');
+    $("#logindiv").hide();
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting',true).log();
+});
